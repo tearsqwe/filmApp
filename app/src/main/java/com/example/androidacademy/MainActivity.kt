8600d6d8490166af7ede5e76f461e49eaf1ff8e5
tearsqwe <chernyakpavel44@gmail.com>
2024-11-17 15:43:01 +0300

    lecture 3 homework w/ fragment

---
+++ app/src/main/java/com/example/androidacademy/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.androidacademy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val fragment: FrameLayout= findViewById(R.id.fragment_container)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if (savedInstanceState==null){
+            val fragment= BlankFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container,fragment)
+                .commit()
+        }
+        fragment.setOnClickListener(){
+            val intent= Intent(this, MovieDetailsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/androidacademy/MovieDetailsActivity.kt
@@ -0,0 +1,25 @@
+package com.example.androidacademy
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_movie_details)
+        val backView:TextView= findViewById(R.id.textView14)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        backView.setOnClickListener(){
+            onBackPressed()
+        }
+    }
+}
